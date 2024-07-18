c0327953e2217dfda45b09caa3c23e0a28fe70b1
alfonsaco <alfonso.rincondev@gmail.com>
2024-07-18 16:57:00 +0200

    Se añade el Day 85

---
+++ Day 85/script.js
@@ -0,0 +1,26 @@
+const container=document.querySelector("main");
+let contenidoHTML='';
+let animaciones=document.createElement("style");
+document.head.appendChild(animaciones);
+let contAnimacion=0;
+let movimiento=0;
+
+for (let i=0; i<174; i++) {
+    contAnimacion++;
+    // Creación de las bolas
+    contenidoHTML+=`<div class="bola" style="animation: subeBaja${contAnimacion} 2s ease-in-out infinite;"></div>`;
+    // Inserto animaciones en el head
+    animaciones.sheet.insertRule(`@keyframes subeBaja${contAnimacion} {
+        from { transform: translateY(${movimiento}px); }
+        50% { transform: translateY(${-movimiento}px); }
+        to { transform: translateY(${movimiento}px); }
+    }`, animaciones.sheet.cssRules.length);
+    
+    if(contAnimacion >= 87) {
+        movimiento-=1+(movimiento*0.01);
+    } else {
+        movimiento+=1+(movimiento*0.01);
+    }
+}
+
+container.innerHTML=contenidoHTML;
