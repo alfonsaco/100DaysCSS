b7e64fb95010908c6faaf90416113a5c48ee91d8
alfonsaco <alfonso.rincondev@gmail.com>
2024-07-23 14:30:50 +0200

    Se añade el Day 38

---
+++ Day 38/scipt.js
@@ -0,0 +1,4 @@
+document.querySelector("main").addEventListener("click", function() {
+    document.querySelector(".forma-1").classList.toggle("aumentar-forma-1");
+    document.querySelector(".forma-2").classList.toggle("aumentar-forma-2");
+});
