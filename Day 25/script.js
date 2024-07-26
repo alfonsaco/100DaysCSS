5ec44e3adbcb5d4b33605058b1e741ff09e110e2
alfonsaco <alfonso.rincondev@gmail.com>
2024-07-26 15:16:35 +0200

    Se añade el Day 25

---
+++ Day 25/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".container").addEventListener("click", function() {
+    document.querySelector(".delante").classList.toggle("perspectiva-delante");
+    document.querySelector(".detras").classList.toggle("perspectiva-detras");
+});
