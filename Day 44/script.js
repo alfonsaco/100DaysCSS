f5c7e40271cbbcb06f2b1a268743a0c954b2d2fd
alfonsaco <alfonso.rincondev@gmail.com>
2024-08-12 17:31:11 +0200

    Se modifican archivos

--- Day 44/script.js
+++ Day 44/script.js
@@ -4,12 +4,15 @@ let contenidoHTML='';
 let anchura=0;
 let altura=0;
 let delay=700;
+let zInd=100;
 
 for (let i=0; i<20; i++) {
     altura+=10;
     anchura+=10;
-    contenidoHTML+=`<div class="cuadrado" style="width: ${anchura}px; height: ${altura}px; animation-delay: ${delay}ms;"></div>`;
+    contenidoHTML+=`<div class="cuadrado" style="width: ${anchura}px; height: ${altura}px; animation-delay: ${delay}ms; z-index: ${zInd};"></div>`;
+    
     delay-=35;
+    zInd--;
 }
 
 contenido.innerHTML+=contenidoHTML;
